1e5a1d976b3bf2dd3738c3801d67daee8126ef32
lqr2007 <hz_124@163.com>
2025-11-06 14:49:46 +0800

    Solve the conflict

--- src/main/java/com/liquor/hardcoreastagesitem/HardcoreAstagesItem.java
+++ src/main/java/com/liquor/hardcoreastagesitem/HardcoreAstagesItem.java
@@ -1,4 +1,4 @@
-package com.liquor.hardcoreastagesitem;
+package com.xiaoyu.hardcoreastagesitem;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.BakedModel;
@@ -7,14 +7,16 @@ import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.Item;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.client.event.RenderGuiEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.server.ServerLifecycleHooks;
+import net.minecraftforge.client.event.RenderGuiEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.server.ServerLifecycleHooks;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
-import net.neoforged.fml.common.Mod;
 
 import java.util.HashMap;
 import java.util.List;
@@ -23,32 +25,27 @@ import java.util.Map;
 @Mod(HardcoreAstagesItem.MODID)
 public class HardcoreAstagesItem {
     public static final String MODID = "hardcoreastagesitem";
-
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public static Map<String, BakedModel> replacedMap = new HashMap<>();
-
+    public static final Map<String, BakedModel> replacedMap = new HashMap<>();
     public static boolean isExecuted = false;
 
-    public HardcoreAstagesItem(IEventBus modEventBus) {
-
+    public HardcoreAstagesItem() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         UnknownItem.register(modEventBus);
 
-        NeoForge.EVENT_BUS.register(RebakeModel.class);
-        NeoForge.EVENT_BUS.addListener(this::onPlayerEnterWorld);
-
+        MinecraftForge.EVENT_BUS.register(RebakeModel.class);
+        MinecraftForge.EVENT_BUS.addListener(this::onPlayerEnterWorld);
     }
 
     @SubscribeEvent
-    private void onPlayerEnterWorld(RenderGuiEvent.Post event) {
+    public void onPlayerEnterWorld(RenderGuiEvent.Post event) {
         if (!isExecuted) {
-
             isExecuted = true;
-
             Minecraft minecraft = Minecraft.getInstance();
             ModelManager modelManager = minecraft.getModelManager();
 
-            ResourceLocation unknownItemResource = ResourceLocation.parse("hardcoreastagesitem:unknown_item");
+            ResourceLocation unknownItemResource = new ResourceLocation(MODID, "unknown_item");
             ModelResourceLocation unknownModel = new ModelResourceLocation(unknownItemResource, "inventory");
             BakedModel replaceModel = modelManager.getModel(unknownModel);
 
@@ -57,14 +54,15 @@ public class HardcoreAstagesItem {
                     List<Item> unknownItems = GetItemList.GetUnknownItemList(onlinePlayer);
                     LOGGER.debug("Get Unknown Item: {}", unknownItems);
 
-                    for (Item itemName : unknownItems) {
-                        ResourceLocation originResource = ResourceLocation.parse(String.valueOf(itemName));
-                        ModelResourceLocation originModel = new ModelResourceLocation(originResource, "inventory");
-                        BakedModel rawModel = modelManager.getModel(originModel);
+                    for (Item item : unknownItems) {
+                        ResourceLocation itemRegistryName = ForgeRegistries.ITEMS.getKey(item);
+                        if (itemRegistryName != null) {
+                            ModelResourceLocation originModel = new ModelResourceLocation(itemRegistryName, "inventory");
+                            BakedModel rawModel = modelManager.getModel(originModel);
 
-                        replacedMap.put(String.valueOf(itemName), rawModel);
-
-                        RebakeModel.replaceModel(originModel, replaceModel, modelManager);
+                            replacedMap.put(itemRegistryName.toString(), rawModel);
+                            RebakeModel.replaceModel(originModel, replaceModel, modelManager);
+                        }
                     }
                 }
             }
